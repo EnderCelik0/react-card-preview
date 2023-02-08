0e681762637fb2150ddc3587ab0b421dc1dfeebf
Ender Çelik <64286011+EnderCelik0@users.noreply.github.com>
2023-02-08 18:56:08 +0300

    add responsivness for desktop version

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -1,33 +1,69 @@
-import previewItem from '../assets/images/drawers.jpg';
-import shareImage from '../assets/images/icon-share.svg';
+import { BsShareFill, BsGithub, BsTwitter } from 'react-icons/bs';
+import { AiFillFacebook } from 'react-icons/ai';
+import { useState } from 'react';
 import personAvatar from '../assets/images/avatar-michelle.jpg';
 
 export default function CardPreview() {
+  const [socialSection, setSocialSection] = useState(false);
+
+  function toggleSocial() {
+    setSocialSection((prev) => !prev);
+  }
+
   return (
     <main className='card-preview-container'>
       <div className='preview-item'></div>
-      <div className='preview-container'>
-        <h4 className='preview-header'>
-          Shift the overall look and feel by adding these wonderful touches to
-          furniture in your home
-        </h4>
-        <p className='preview-info'>
-          Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I've got some simple tips to help
-          you make any room feel complete.
-        </p>
-      </div>
-      <div className='person-info'>
-        <div className='person-flex'>
-          <img src={personAvatar} className='person-avatar' />
-          <div className='person-publish'>
-            <p className='person-name'>Michelle Appleton</p>
-            <p className='publish-date'>28 Jun 2020</p>
+      <div className='preview'>
+        <div className='preview-container'>
+          <h4 className='preview-header'>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h4>
+          <p className='preview-info'>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I've got some simple tips to help
+            you make any room feel complete.
+          </p>
+        </div>
+        {socialSection ? (
+          <div className='social-section'>
+            <div className='social-flex'>
+              <h4 className='text-share'>SHARE</h4>
+              <div className='social-icons'>
+                <a href='github.com/EnderCelik0'>
+                  <BsGithub className='github' />
+                </a>
+                <a>
+                  <BsTwitter className='twitter' />
+                </a>
+                <a>
+                  <AiFillFacebook className='facebook' />
+                </a>
+              </div>
+            </div>
+            <div className='share-img-social' onClick={toggleSocial}>
+              <BsShareFill className='share' />
+            </div>
           </div>
-        </div>
-        <div className='share-img'>
-          <img src={shareImage} />
-        </div>
+        ) : (
+          <div className='person-info'>
+            <div className='person-flex'>
+              <img
+                src={personAvatar}
+                className='person-avatar'
+                alt='person profile picture'
+              />
+              <div className='name-publish'>
+                <p className='person-name'>Michelle Appleton</p>
+                <p className='publish-date'>28 Jun 2020</p>
+              </div>
+            </div>
+
+            <div className='share-img' onClick={toggleSocial}>
+              <BsShareFill className='share' />
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
